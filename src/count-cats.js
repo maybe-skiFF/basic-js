3dd4063f80a888e7d6d9b4cc2344fed6bb866f52
maybe-skiFF <den.skiff190@gmail.com>
2023-04-16 10:39:10 +0300

    feat: complete count-cats and dream-team tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let countCat = 0;
+
+  matrix.forEach(el => {
+    el.forEach(subEl => {
+      if (subEl === '^^') countCat++
+    })
+  })
+
+  return countCat;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let resArr = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+
+  members.forEach((el) => {
+    if(typeof el === 'string') {
+        el = el.trim();
+        resArr.push(el[0].toUpperCase());
+    }
+  })
+
+
+  return resArr.sort().join('');
 }
 
 module.exports = {
