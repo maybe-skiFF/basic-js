e06233f6624fc94c3830f0c175c724b918857d7a
maybe-skiFF <den.skiff190@gmail.com>
2023-12-19 21:36:55 +0300

    feat: add solution get-email-domain task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,9 +12,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-
   let res = 0;
   let str1LikeArr = s1.split('');
   let str2LikeArr = s2.split('');
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let res = [];
+  const emailLikeArr = email.split('');
+
+  emailLikeArr.forEach((item, index) => {
+    if (item === '@') {
+      res = emailLikeArr.slice(index + 1)
+    }
+  })
+
+return res.join('');
 }
 
 module.exports = {
