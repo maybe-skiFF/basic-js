7c01b4d489b06e5afe713076ee20a4b0ed449288
maybe-skiFF <den.skiff190@gmail.com>
2023-12-21 22:34:00 +0300

    feat: add solution extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let buffer = [];
+  let concatBuffer = [];
+  let resStr = '';
+
+  if (options.hasOwnProperty('additionRepeatTimes')) {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      buffer.push(String(options.addition));
+    }
+  } else {
+    if (options.hasOwnProperty('addition')) {
+      buffer.push(String(options.addition));
+    }
+  }
+
+  if (options.hasOwnProperty('additionSeparator')) {
+    buffer = buffer.join(options.additionSeparator);
+  } else {
+    buffer = buffer.join('|');
+  }
+
+  resStr = `${str}${buffer}`;
+
+  if (options.hasOwnProperty('repeatTimes')) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      concatBuffer.push(resStr);
+    }
+  } else {
+    concatBuffer.push(resStr);
+  }
+
+  if (options.hasOwnProperty('separator')) {
+    concatBuffer = concatBuffer.join(options.separator)
+  } else {
+    concatBuffer = concatBuffer.join('+')
+  }
+
+  return concatBuffer;
 }
 
 module.exports = {
